f668ac310c9458ebb7aa05aed0cd151a9fcba880
OneShepForward <shephardn@gmail.com>
2022-01-04 15:31:37 -0600

    Update 2

--- phase-3-project-frontend-app/src/App.js
+++ phase-3-project-frontend-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This is Eve and Nick's Frontend App
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
