7c5cacee7a4137696f803c88a5b057075ee49717
Eve Kozina <eve.kozina@gmail.com>
2022-01-05 16:57:24 -0500

    Added Tinder-like pile

---
+++ phase-3-project-frontend-app/src/TinderPile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TinderCard from 'react-tinder-card'
+
+
+function TinderPile() {
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + direction)
+    }
+      
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+    return (
+        <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
+      )
+}
+
+export default TinderPile
