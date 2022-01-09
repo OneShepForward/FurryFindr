098a55d13317c436a8630d6c132648a02db13a73
Nicholas Shephard <shephardn@gmail.com>
2022-01-09 17:01:33 -0600

    User View added to show matched pets

---
+++ phase-3-project-frontend-app/src/UserView.jsx
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from "react";
+
+function UserView( { activeUser, pets } ) {
+    // const [petList, setPetList] = useState()
+    // console.log("UserView says the Active user is:", user)
+
+    // useEffect(()=> {
+    //     fetch(`/pets/${activeUser.id}`)
+    //     .then(res => res.json())
+    //     .then(petsToRender => setPetList(petsToRender))
+    //   }, [])
+
+//    const renderMatches = pets.map((pet) => {
+//        return <div
+//             key = {pet.id}
+//             className="listedPet"
+//        >
+//         {pet.name}
+//        </div>
+//    });
+console.log(pets)
+
+   function renderMatches () { 
+       return pets.map((pet) => {
+        return <div
+            key = {pet.id}
+            className="listedPet"
+       >
+        {pet.name}
+       </div>
+   })
+}
+
+//    const renderMatches = allUserData.map((user) => {
+//         return <div
+//             key={user.id} 
+//             className={setUserClassName(user.id)}
+//             onClick={() => userClicked(user)}
+//         >
+//         {user.name}
+//     </div>        
+// });
+   
+    return (
+        <div className='user-view'>
+            <h1>{activeUser.name}'s Matched Pets</h1> 
+            {/* check that pets has been defined */}
+            {pets ? 
+                // check that there is at least one match
+                pets.length > 0 ? 
+                    (
+                    renderMatches()
+                    )
+                    :
+                    (
+                    <h2>"No matches yet. Find a furry friend!"</h2>
+                    )
+            :
+                "Loading..."
+            }  
+        </div>
+    );
+}
+
+export default UserView
